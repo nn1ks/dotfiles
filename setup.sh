d6cddb8075a7221a59bbc4765fc1238edf99112c
Niklas Sauter <niklas.sauter@gmx.de>
2020-12-15 01:21:03 +0100

    setup: Fix setup script

--- setup.sh
+++ setup.sh
@@ -11,13 +11,12 @@ mkdir "$HOME/src"
 git clone https://gitlab.redox-os.org/redox-os/ion "$HOME/src/ion"
 cd "$HOME/src/ion"
 mkdir custom-bin
-echo "(manifest->specifications\n" > manifest.scm
-echo "  '(\"gcc-toolchain\")" >> manifest.scm
+echo "(specifications->manifest" > manifest.scm
+echo "  '(\"gcc-toolchain\"))" >> manifest.scm
 guix environment -m manifest.scm
 ln -s "$(which gcc)" custom-bin/cc
 export PATH="$HOME/src/ion/custom-bin:$PATH"
-cargo build --release
-cargo install
+cargo install --path .
 
 git clone https://github.com/rust-analyzer/rust-analyzer "$HOME/src/rust-analyzer"
 cd "$HOME/src/rust-analyzer"
