4d90bd4ba113497b44e72a74d879ec84b6b20012
Niklas Sauter <niklas@n1ks.net>
2021-01-23 13:23:49 +0100

    setup: Remove rust-analyzer from setup script

--- setup.sh
+++ setup.sh
@@ -5,9 +5,3 @@ set -x
 dconf load / < "$HOME/.dconf-dump"
 
 git clone https://github.com/alexherbo2/plug.kak "$HOME/.config/kak/autoload/plugins/plug"
-
-mkdir "$HOME/src"
-
-git clone https://github.com/rust-analyzer/rust-analyzer "$HOME/src/rust-analyzer"
-cd "$HOME/src/rust-analyzer"
-cargo xtask install --server
