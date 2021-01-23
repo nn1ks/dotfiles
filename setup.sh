821e9445ca363fd5e30863981546db031a081183
Niklas Sauter <niklas@n1ks.net>
2021-01-23 13:21:33 +0100

    ion: Remove ion

--- setup.sh
+++ setup.sh
@@ -8,16 +8,6 @@ git clone https://github.com/alexherbo2/plug.kak "$HOME/.config/kak/autoload/plu
 
 mkdir "$HOME/src"
 
-git clone https://gitlab.redox-os.org/redox-os/ion "$HOME/src/ion"
-cd "$HOME/src/ion"
-mkdir custom-bin
-echo "(specifications->manifest" > manifest.scm
-echo "  '(\"gcc-toolchain\"))" >> manifest.scm
-guix environment -m manifest.scm
-ln -s "$(which gcc)" custom-bin/cc
-export PATH="$HOME/src/ion/custom-bin:$PATH"
-cargo install --path .
-
 git clone https://github.com/rust-analyzer/rust-analyzer "$HOME/src/rust-analyzer"
 cd "$HOME/src/rust-analyzer"
 cargo xtask install --server
